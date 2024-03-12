70cd03a33cf9b18e231808ea5b016cb37e4bdefa
Carlos <carloseduardo.eduleal@gmail.com>
2024-03-12 01:28:41 -0300

    modiicado o return do else

--- teste_pri.py
+++ teste_pri.py
@@ -3,6 +3,6 @@ def par(t):
     if t%2 == 0:
         return (f"{t} é par")
     else:
-        return "É impar"
+        return (f"{t} é impar")
 numero = int(input("Digite um numero: "))
 print (f"{par(numero)}") 
